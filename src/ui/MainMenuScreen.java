981aa2da9143195bb97d4f19ad3611a2d64eb3e9
evdailyyy <evelyn.silva@my.utsa.edu>
2025-10-27 01:42:42 -0500

    added to main menu screen
    i added the exit option

--- src/ui/MainMenuScreen.java
+++ src/ui/MainMenuScreen.java
@@ -7,7 +7,10 @@ import java.util.Scanner;
 
 public class MainMenuScreen extends Screen {
     public static void showMainMenuScreen() {
-        if(LoginScreen.getIsAccount()) {
+        //we need to loop until users exits
+            boolean keepRunning = true;
+
+            while(keepRunning ) {
             displayHeader("Main Menu", "Select any Option");
             System.out.printf("%-40s\n", "===========================================");
             System.out.printf("\t\t\t\t%-40s\n", "Main Menu");
@@ -15,14 +18,21 @@ public class MainMenuScreen extends Screen {
             System.out.printf("\t\t%-40s\n", "[1] View Account");
             System.out.printf("\t\t%-40s\n", "[2] View Music Selection");
             System.out.printf("\t\t%-40s\n", "[3] Rate A Song");
+            System.out.printf("\t\t%-40s\n", "[4] Exit");
+            System.out.printf("%-40s\n", "===========================================");
+            System.out.print("\nEnter your choice: ");  
+            handleUserSelection(currentUser); 
+
+
         }
     }
     private static void handleUserSelection(User currentUser) {
-        int choice = InputValidator.readIntRange(1, 3);
+        int choice = InputValidator.readIntRange(1, 4);
         MainMenuOptions option = MainMenuOptions.values()[choice - 1];
         performMainMenuOption(option, currentUser);
     }
 
+    //switch case for users choice in menu option
     public static void performMainMenuOption(MainMenuOptions mainMenuOptions, User currentUser) {
         switch(mainMenuOptions){
             case eViewAccount:
@@ -34,6 +44,11 @@ public class MainMenuScreen extends Screen {
             case eRateSong:
                 Rate.showRateScreen(currentUser);
                 break;
+            case eExit:
+            System.out.println("Goodbye, Thank you!");
+            keepRunning = false;
+            break;
+                
         }
     }
     private static void showViewAccount(User currentUser) {
@@ -47,7 +62,10 @@ public class MainMenuScreen extends Screen {
         new Scanner(System.in).nextLine(); // wait for user to continue
     }
 
-    private static void showMusicSelection(){ System.out.println("Show Music Selection..."); }
-
+    private static void showMusicSelection(){ 
+        System.out.println("Show Music Selection..."); 
+        System.out.println("\nPress Enter to return to Main Menu...");
+        new Scanner(System.in).nextLine();
+    }
 
 }
